e8846818676202649fb8be7eafe35fea0cf29507
ooluwgb <111203890+ooluwgb@users.noreply.github.com>
2022-08-28 17:10:47 -0500

    Commented out installation of jenkins

--- auto.sh
+++ auto.sh
@@ -13,51 +13,51 @@ echo
 #INSTALL DOCKER ON UBUNTU V20.04
 echo Installing docker on UBuntu Machine.........
 sudo apt-get install -y docker.io
-sleep 4
-echo
-echo
-echo
+#sleep 4
+#echo
+#echo
+#echo
 
 #INSTALL JENKINS 
 #Install all dependencies for jenkins
-echo Installing all dependencies needed for jenkins to run...........
-sudo apt-get install -y default-jre
-sudo apt-get install -y default-jdk
-sleep 4
-echo
-echo
-echo
+#echo Installing all dependencies needed for jenkins to run...........
+#sudo apt-get install -y default-jre
+#sudo apt-get install -y default-jdk
+#sleep 4
+#echo
+#echo
+#echo
 #Install jenkins package
 
-echo Installing Jenkins ........
+#echo Installing Jenkins ........
 
-wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
-sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
-sudo apt update
-sudo apt install -y jenkins
-sleep 4
-echo
-echo
-echo
+#wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+#sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+#sudo apt update
+#sudo apt install -y jenkins
+#sleep 4
+#echo
+#echo
+#echo
 #STARTING  UP JENKINS PACKAGE
-echo All task COMPLETED!!!!
-echo
-echo Jenkins Package should be starting in a sec...
-sleep 3
-echo Jenkins Package is getting started....
-sudo systemctl start jenkins
-echo
-echo
-echo
+#echo All task COMPLETED!!!!
+#echo
+#echo Jenkins Package should be starting in a sec...
+#sleep 3
+#echo Jenkins Package is getting started....
+#sudo systemctl start jenkins
+#echo
+#echo
+#echo
 
 
 #ADD JENKINS TO DOCKER GROUP - SO THAT JENKINS CAN HAVE ACCESS TO USE DOCKER
-echo Jenkins user added to docker group
-sudo usermod -aG docker jenkins
+#echo Jenkins user added to docker group
+#sudo usermod -aG docker jenkins
 
 
-echo JOB SUCCESSFULLY COMPLETED!!!!!
-echo BYE
+#echo JOB SUCCESSFULLY COMPLETED!!!!!
+#echo BYE
 
 
 
